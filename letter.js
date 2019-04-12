04fa1664177139e0613b659c7367a2d1d80a5fbe
robertmadril <robertmmadril@gmail.com>
2019-04-11 18:29:28 -0700

    add logic to letter functions and require letter in word

--- letter.js
+++ letter.js
@@ -1,11 +1,19 @@
-var Letter = function(letter, ) {
+var Letter = function(letter) {
     this.letter = letter;
     this.guessed = false;
     this.checkLetter = function() {
-        
+        if (this.guessed) {
+            return this.letter;
+        }
+        else {
+            return "__";
+        }
     }
-    this.updateBoolean = function() {
-
+    this.updateBoolean = function(char) {
+        if (char === this.letter) {
+            this.guessed = true;
+        }
     }
 }
 
+module.exports = Letter;
---
+++ word.js
@@ -0,0 +1,4 @@
+var Letter = require("./letter.js");
+
+
+
